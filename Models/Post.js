118c3efe85690644429db6d8d6fb0d1c807b004b
Alejandra179 <alejandra.maza1@gmail.com>
2023-09-19 09:49:14 -0300

    modelo actualizado y conexion a base de datos realizada

--- Models/Post.js
+++ Models/Post.js
@@ -1,5 +1,8 @@
-const {Sequelize, Model, DataTypes} = rquire('sequelize');
-const sequelize = new Sequelize('');
+const {Sequelize, Model, DataTypes} = require('sequelize');
+const sequelize = new Sequelize('my_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+  });
 
 
 class Post extends Model{}
@@ -25,5 +28,11 @@ Post.init({
     }
 
 )
-
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 module.exports = Post
--- index.js
+++ index.js
@@ -27,3 +27,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
